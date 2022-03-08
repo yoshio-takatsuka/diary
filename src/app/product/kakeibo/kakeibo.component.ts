ff66ce71acfca363520fc7d3bd3992c0ce535840
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2022-03-09 07:27:00 +0900

    修正 20220309

--- src/app/product/kakeibo/kakeibo.component.ts
+++ src/app/product/kakeibo/kakeibo.component.ts
@@ -21,43 +21,7 @@ export class KakeiboComponent {
 
   // kakeibo の初期値
   /** ユーザ情報登録フォーム */
-  myKakeibo: FormGroup;
-
-  // p_date = new FormControl('', [
-  //   Validators.required,
-  //   Validators.pattern('^(?!([02468][1235679]|[13579][01345789])000229)(([0-9]{4}(01|03|05|07|08|10|12)(0[1-9]|[12][0-9]|3[01]))|([0-9]{4}(04|06|09|11)(0[1-9]|[12][0-9]|30))|([0-9]{4}02(0[1-9]|1[0-9]|2[0-8]))|([0-9]{2}([02468][048]|[13579][26])0229))$')
-  // ]);
-
-  // kakebos = new FormArray([
-
-  // ])
-  // kakeboTime = new FormControl('', [
-  // ]);
-  // kakeboKamoku = new FormControl('', [
-  // ]);
-  // kakeboDetail = new FormControl('', [
-  // ]);
-  // kakeboEtc = new FormControl('', [
-  // ]);
-  // kakeboPayKbn = new FormControl('', [
-  // ]);
-  // kakeboKingaku = new FormControl(0, [
-  // ]);
-  // chek1 = new FormControl(false, [
-  // ]);
-  // myKakeibox = this.builder.group({
-  //   p_date: this.p_date,
-  //   kakeibos: this.kekeibos,
-  //   kakeboTime: this.kakeboTime,
-  //   kakeboKamoku: this.kakeboKamoku,
-  //   kakeboEtc: this.kakeboEtc,
-  //   kakeboPayKbn: this.kakeboPayKbn,
-  //   kakeboKingaku: this.kakeboKingaku,
-
-
-  // });
-
-
+  myKakeiboForm: FormGroup;
 
   constructor(
     private productService: ProductService,
@@ -65,18 +29,24 @@ export class KakeiboComponent {
     private builder: FormBuilder
 
   ) {
+
+    this.myKakeiboForm = this.builder.group({
+        p_date: [''],
+        kakeibos: this.builder.array([
+          this.createmyKakeibo()
+        ],Validators.required)
+
+    });
+
   }
 
 
   ngOnInit() {
 
     // 最初に動く処理
-    debugger
-    this.myKakeibo = this.createmyKakeibo();
-    debugger
-
-
-
+    // debugger
+    // this.myKakeibo = this.createmyKakeibo();
+    // debugger
   }
   /**
   * formArrayName を取得するために必要な Getter
@@ -84,22 +54,16 @@ export class KakeiboComponent {
   * @return 家計簿情報 FormArray
   */
   createmyKakeibo(): FormGroup {
-    return this.builder.group({
-      p_date: [''],
-      kakeibos: this.builder.array([])
-    });
-  }
-
-  get userSkillForm(): FormGroup {
-    return this.builder.group({
+    return  this.builder.group({
       kakeiboTime: [''],
-      kakeibokamoku: [''],
+      kakeiboKamoku: [''],
       kakeboDetail: [''],
       kakeboEtc: [''],
       kakeboPayKbn: [''],
       kakeboKingaku: [''],
     });
   }
+
 
 
 
@@ -110,26 +74,35 @@ export class KakeiboComponent {
   */
   get kekeibos(): FormArray {
     debugger
-    return this.myKakeibo.get('kekeibos') as FormArray;
+    // console.log(JSON.stringify(this.myKakeiboForm, null, 2));
+
+    return this.myKakeiboForm.get('kekeibos') as FormArray;
   }
+
+  // get kakeibos(): FormArray {
+  //   return <FormArray> this.myKakeiboForm.get('kakeibos');
+ 
+  // }
 
   /**
    * 家計簿情報 入力フォームを1行追加する
    */
   addKakeibo() {
     // Getter を用意したいので「this.kekeibos」でアクセス可能
-    this.kekeibos.push(this.builder.group({
-      kakeboTime: '',     //時刻
-      kakeboKamoku: '',   //科目
-      kakeboDetail: '',   //詳細
-      kakeboEtc: '',      //その他
-      kakeboPayKbn: '',   //支払方法
-      kakeboKingaku: '',  //金額
-    }));
+    debugger
+    this.kekeibos.push(this.createmyKakeibo());
+  }
+  /**
+   * 家計簿情報 入力フォームを1行削除する
+   */
+   removeKakeibo(index: number) {
+    // Getter を用意したいので「this.kekeibos」でアクセス可能
+    debugger
+    this.kekeibos.removeAt(index);
   }
 
   // formの日記、家計簿、スキル情報を更新する 
-  registerKakeibo(myKakeibo) {
+  registerKakeibo(myKakeiboForm) {
 
     let res = confirm("登録してもいいですかー？");
     if (res == true) {
@@ -140,7 +113,7 @@ export class KakeiboComponent {
       return;
     }
 
-    this.productService.kekeibo(myKakeibo).subscribe(
+    this.productService.kekeibo(myKakeiboForm).subscribe(
       (result) => {
         console.log("Success!")
         // this.router.navigate(['/login'])
@@ -153,9 +126,9 @@ export class KakeiboComponent {
     )
   }
   // 該当日付の日記情報を取得してくる 
-  serch(myKakeibo) {
+  serch(myKakeiboForm) {
 
-    const productsObservable = this.productService.getDairy(myKakeibo.p_date.value)
+    const productsObservable = this.productService.getDairy(myKakeiboForm.p_date.value)
     productsObservable.subscribe(
       (result) => {
         this.checkValue1 = true

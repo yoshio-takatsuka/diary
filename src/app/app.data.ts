272b1676f68199ff01ac821a7ba4f7c84f98fd49
yoshio.takatsuka <yoshio.takatsuka@gamil.com>
2022-03-21 07:33:57 +0900

    修正　20220321

--- src/app/app.data.ts
+++ src/app/app.data.ts
@@ -15,19 +15,20 @@ export type KouzaItem = {
 export class DataService {
     getKamokuData() {
         return <KamokuItem[]>[
-            { id:  1, kamoku: '01:水道光熱費',kbn:"出金"  },
-            { id:  2, kamoku: '02:通信料',kbn:"出金" },
-            { id:  3, kamoku: '03:習い事',kbn:"出金" },
-            { id:  4, kamoku: '04:交通費',kbn:"出金"},
-            { id:  5, kamoku: '05:食費',kbn:"出金" },
-            { id:  6, kamoku: '06:外食費' ,kbn:"出金"},
-            { id:  7, kamoku: '07:日用品費' ,kbn:"出金"},
-            { id:  8, kamoku: '08:医療費' ,kbn:"出金"},
-            { id:  9, kamoku: '09:トリニータ',kbn:"出金" },
-            { id: 10, kamoku: '10:交際費',kbn:"出金" },
-            { id: 11, kamoku: '11:娯楽費',kbn:"出金" },
-            { id: 12, kamoku: '12:口座' ,kbn:"出金"},
-            { id: 13, kamoku: '13:入金',kbn:"入金" },
+            { id:  1, kamoku: '出:水道光熱費',kbn:"出"},
+            { id:  2, kamoku: '出:通信料',kbn:"出" },
+            { id:  3, kamoku: '出:習い事',kbn:"出" },
+            { id:  4, kamoku: '出:交通費',kbn:"出"},
+            { id:  5, kamoku: '出:食費',kbn:"出" },
+            { id:  6, kamoku: '出:外食費' ,kbn:"出"},
+            { id:  7, kamoku: '出:日用品費' ,kbn:"出"},
+            { id:  8, kamoku: '出:医療費' ,kbn:"出"},
+            { id:  9, kamoku: '出:トリニータ',kbn:"出" },
+            { id: 10, kamoku: '出:交際費',kbn:"出" },
+            { id: 11, kamoku: '出:娯楽費',kbn:"出" },
+            { id: 12, kamoku: '出:口座' ,kbn:"出"},
+            { id: 13, kamoku: '入:入金',kbn:"入" },
+            { id: 14, kamoku: '残:残高',kbn:"残" },
         ];
     }
     getKouzaData() {
